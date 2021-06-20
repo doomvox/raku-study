18bb9fde397869c8aaef99abf9cf0edb5faeecec
Joseph Brenner <doom@kzsu.stanford.edu>
2021-06-20 14:25:18 -0700

    wip

--- bin/2021jun20/defined_or_in_perl.pl
+++ bin/2021jun20/defined_or_in_perl.pl
@@ -27,7 +27,7 @@ use Data::Dumper;
 
 sub dostuff {
 #  my $arg = shift || 'default'; 
-  my $arg = shift // 'default';   # is shift defined?  
+  my $arg = shift // 'default';   # is shift defined?  if not, use 'default'
   say $arg;
 }
 
