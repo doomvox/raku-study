74e32fe8320226619b0a0d41bc2db6a4cdedcfef
Joseph Brenner <doom@kzsu.stanford.edu>
2021-07-11 13:32:56 -0700

    wip

--- notes/talks/dualmap/slide_definitions.el
+++ notes/talks/dualmap/slide_definitions.el
@@ -49,6 +49,7 @@
     ( E . "/home/doom/End/Cave/DataMathRaku/Wall/Data-MapDual/t/02-dualmap.t" )
     ( F . "/home/doom/End/Cave/DataMathRaku/Wall/Data-MapDual/t/03-dualmap_codeblocks.t" )
     ( G . "/home/doom/End/Cave/DataMathRaku/bin/perl_sort_demo.pl" )
+    ( H . "/home/doom/End/Cave/DataMathRaku/bin/perl_sort_demo.pl" )
    ))
 
     ;; a further subject:
