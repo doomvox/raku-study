0e57d7ce9136ddeb531d2d53b719ea3125d63bca
Joseph Brenner <doom@kzsu.stanford.edu>
2021-09-26 11:32:00 -0700

    wip

---
+++ bin/2021sep26/playing_with_dualvars.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/env perl
+#                                      2021/09/26 11:31:58
+
+=head1 NAME
+
+playing_with_dualvars.pl
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+Stub documentation for playing_with_dualvars.pl,
+created by template.el.
+
+It looks like the author of this script was negligent
+enough to leave the stub unedited.
+
+=cut
+
+use 5.10.0;
+use warnings;
+use strict;
+$|=1;
+use Data::Dumper;
+
+
+
+
+
+
+
+
+__END__
+
+=head1 AUTHOR
+
+Joseph Brenner, E<lt>doom@fandango.obsidianrook.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2021 by Joseph Brenner
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.0 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
