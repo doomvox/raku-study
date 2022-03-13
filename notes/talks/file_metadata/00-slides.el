f37a1b024aad4de83c6decf35112e7a7e3d87a78
Joseph Brenner <doom@kzsu.stanford.edu>
2022-03-13 12:26:05 -0700

    wip

---
+++ notes/talks/file_metadata/00-slides.el
@@ -0,0 +1,59 @@
+;;; 00-slides.el --- file metadata talkette slids
+
+;; Copyright 2022 Joseph Brenner
+;;
+;; Author: doom@kzsu.stanford.edu
+;; Version: $Id: 00-slides.el,v 0.0 2022/03/13 19:25:19 doom Exp $
+;; Keywords: 
+;; X-URL: not distributed yet
+;; License: the same as your GNU emacs (see below)
+
+;;; Commentary:
+
+;; 
+
+;; Put this file into your load-path and the following into your ~/.emacs:
+
+;;   (require 'slip_and_slide)
+;;   (slip-and-slide-initialize)
+;;   (slip-and-slide-define-standard-keymapping "\M-o")
+
+;;   (require '00-slides)
+
+
+
+;;; Code:
+
+(provide '00-slides)
+(require 'cl-lib)
+
+
+;;;;##########################################################################
+;;;;  User Options, Variables
+;;;;##########################################################################
+
+
+
+
+
+
+
+;; LICENSE
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the same terms as the version of GNU Emacs you intend to use it with.
+
+;; At present, GNU Emacs is under the GNU General Public License version 3
+;; or (at your option) any later version.  This license is as published by
+;; the Free Software Foundation.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;;; 00-slides.el ends here
