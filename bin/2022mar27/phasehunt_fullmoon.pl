ed52f568782bfb7097c6b3758add1460c42be197
Joseph Brenner <doom@kzsu.stanford.edu>
2022-03-27 15:25:39 -0700

    wip

--- bin/2022mar27/phasehunt_fullmoon.pl
+++ bin/2022mar27/phasehunt_fullmoon.pl
@@ -31,7 +31,7 @@ my @phases = phasehunt();
 #print "First quarter = ", scalar(localtime($phases[1])), "\n";
 print "Full moon     = ", scalar(localtime( $phases[2]) ), "\n";
 #print "Last quarter  = ", scalar(localtime($phases[3])), "\n";
-print "New Moon      = ", scalar(localtime( $phases[4]) ), "\n";
+# print "New Moon      = ", scalar(localtime( $phases[4]) ), "\n";
 
 
 
