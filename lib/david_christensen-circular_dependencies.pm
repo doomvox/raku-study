3bfa97120d868fe95b3e2e8066ecc0f6b85514d4
Joseph Brenner <doom@kzsu.stanford.edu>
2022-06-06 00:14:46 -0700

    wip

--- lib/david_christensen-circular_dependencies.pm
+++ lib/david_christensen-circular_dependencies.pm
@@ -48,11 +48,12 @@ The key points are:
 
 =over
 
-  =item First -- declare @EXPORT_OK, but do not define/ initialize it (line 14).
+=item *
+          First -- declare @EXPORT_OK, but do not define/ initialize it (line 14).
+=item * 
+          Next -- define/ initialize @EXPORT_OK in a BEGIN block (lines 16-20).
 
-  =item Next -- define/ initialize @EXPORT_OK in a BEGIN block (lines 16-20).
-
-  =item Finally -- 'use parent' to inherit from Exporter (lines 22, 23, and 25).
+=item *  Finally -- 'use parent' to inherit from Exporter (lines 22, 23, and 25).
 
 =back
 
