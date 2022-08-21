14402448466ec98d46ee531f4c70222ee65d040b
Joseph Brenner <doom@kzsu.stanford.edu>
2022-08-21 04:36:18 -0700

    wip

--- bin/2022aug21/10-modify_substr_via_subst.pl
+++ bin/2022aug21/10-modify_substr_via_subst.pl
@@ -41,7 +41,6 @@ use Data::Dumper;
   say "will skip from: $skip1 to $skip2";
   say substr( $str, $skip1, $skip2 ); # 
 
-
   substr( $str, 0, $skip1 )            =~ s{baby}{respected colleague}g;
   substr( $str, $skip2, length($str) ) =~ s{baby}{respected colleague}g;
 
