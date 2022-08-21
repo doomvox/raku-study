46567e44478b25eb6f6a3ba40ed5b721c81d577d
Joseph Brenner <doom@kzsu.stanford.edu>
2022-08-21 05:53:37 -0700

    wip

--- bin/2022aug21/10-modify_substr_via_subst.pl
+++ bin/2022aug21/10-modify_substr_via_subst.pl
@@ -26,7 +26,7 @@ as well as a number of traps lurking in even the simplest programming
 task.
 
 The task is that starting with the quote-of-the-week, replace the
-condescending use of "honey" with a phrase more likely to be accepted
+condescending term "honey" with a phrase more likely to be accepted
 in our new era of modern englightenment.  
 
 
