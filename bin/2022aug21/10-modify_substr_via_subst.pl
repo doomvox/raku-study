4a026821b4e30b7f3799a9f6742da877d64220be
Joseph Brenner <doom@kzsu.stanford.edu>
2022-08-21 04:16:56 -0700

    wip

---
+++ bin/2022aug21/10-modify_substr_via_subst.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/env perl
+#                                      2022/08/21 11:14:20
+
+=head1 NAME
+
+10-modify_substr_via_subst.pl
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+Stub documentation for 10-modify_substr_via_subst.pl,
+created by template.el.
+
+It looks like the author of this script was negligent
+enough to leave the stub unedited.
+
+=cut
+
+use 5.10.0;
+use warnings;
+use strict;
+$|=1;
+use Data::Dumper;
+
+
+{
+  my $str =
+    q{Look out baby, cause I'm using technology
+      Ain't got time to make no apology.
+      FLAGGED: usage "baby"
+     };
+
+  my $a = 25;
+  my $off = 19;
+  my $b = $a + $off;
+
+  say substr( $str, $a, $off ); # can be so expressed
+
+  substr( $str, $a, $off ) =~ s{be}{BE}g;
+
+  say "---";
+  say $str;
+
+}
+
+
+
+
+
+
+__END__
+
+=head1 AUTHOR
+
+Joseph Brenner, E<lt>doom@debian-BULLSEYE-live-builder-AMD64E<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2022 by Joseph Brenner
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.0 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
