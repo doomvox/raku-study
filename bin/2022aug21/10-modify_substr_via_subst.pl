770f1e780697cf08f422cc068705bdc2458f8564
Joseph Brenner <doom@kzsu.stanford.edu>
2022-08-21 06:02:46 -0700

    wip

--- bin/2022aug21/10-modify_substr_via_subst.pl
+++ bin/2022aug21/10-modify_substr_via_subst.pl
@@ -27,8 +27,27 @@ task.
 
 The task is that starting with the quote-of-the-week, replace the
 condescending term "honey" with a phrase more likely to be accepted
-in our new era of modern englightenment.  
+in our new era of modern englightenment.  There's also a line 
+beginning with the key phrase "FLAGGED:" that we want to exclude 
+from this change.  The task is done by 
 
+   (1) locating where the FLAGGED line begins
+
+   (2) locating where the FLAGGED line ends
+
+   (3) doing a s/// on the chunk *after* the FLAGGED line
+
+   (4) doing a s/// on the chunk *before* the FLAGGED line
+   
+
+Some notes:
+
+  o  (3) and (4) are done in the reverse order of what you might expect so 
+     that the locations of things don't change due to the previous s/// change
+
+  o  Everytime I reach for substr (which isn't often) I expect that both 
+     numeric arguments are *positions*, really the first is a position, the 
+     second is a length
 
 
 
