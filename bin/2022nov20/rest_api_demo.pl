616ba669d80c2359502deba3034dc1011b77f867
Joseph Brenner <doom@kzsu.stanford.edu>
2022-11-20 12:04:28 -0800

    wip

---
+++ bin/2022nov20/rest_api_demo.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/env perl
+#                                      2022/11/20 20:04:22
+
+=head1 NAME
+
+rest_api_demo.pl
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+Stub documentation for rest_api_demo.pl,
+created by template.el.
+
+It looks like the author of this script was negligent
+enough to leave the stub unedited.
+
+=cut
+
+use 5.10.0;
+use warnings;
+use strict;
+$|=1;
+use Data::Dumper;
+
+
+
+
+
+
+__END__
+
+=head1 AUTHOR
+
+Joseph Brenner, E<lt>doom@debian-BULLSEYE-live-builder-AMD64E<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2022 by Joseph Brenner
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.0 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
