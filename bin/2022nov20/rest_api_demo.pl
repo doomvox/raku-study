aeab1fa12fb49eb8ee890b6e040eccee9aa1c1f6
Joseph Brenner <doom@kzsu.stanford.edu>
2022-11-20 12:08:03 -0800

    wip

--- bin/2022nov20/rest_api_demo.pl
+++ bin/2022nov20/rest_api_demo.pl
@@ -10,11 +10,15 @@ rest_api_demo.pl
 
 =head1 DESCRIPTION
 
-Stub documentation for rest_api_demo.pl,
-created by template.el.
+I'm trying to get the example Perl5 code from the bottom of this REST-API page to work:
 
-It looks like the author of this script was negligent
-enough to leave the stub unedited.
+https://rest.ensembl.org/documentation/info/archive_id_post
+
+I can get the example curl, wget, and R code to work from the same
+page, so I know the server (https://rest.ensembl.org) is up and
+running. Below is the code, cut-and-pasted. I had to install the JSON
+module. All I get back is the string "Failed!" while curl, wget, and R
+all return actual data
 
 =cut
 
