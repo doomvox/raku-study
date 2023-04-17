d7d24d55f332281ae2b0c2e61f625d728c76a56a
Joseph Brenner <doom@kzsu.stanford.edu>
2023-04-16 17:04:30 -0700

    wip

--- bin/2023apr16/unixstackexchange_fortran_d_to_scientific_e_and_filter_awk.sh
+++ bin/2023apr16/unixstackexchange_fortran_d_to_scientific_e_and_filter_awk.sh
@@ -1,1 +1,3 @@
+# solution posted to unixstackexchange in awk
+# https://unix.stackexchange.com/a/742940/227738
 awk 'NF>3{gsub(/D/,"E"); for (i=1; i<=NF; i++) if ($i != $i+0) print "not a number:", $i}' file
