fee2e2e753b61c1f6e3c8c3f4f133aa613bcf3bb
Joseph Brenner <doom@kzsu.stanford.edu>
2023-10-08 13:14:44 -0700

    wip

---
+++ bin/2023oct08/somecode.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/env perl
+#                                      2023/10/08 20:14:29
+
+=head1 NAME
+
+somecode.pl
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+Stub documentation for somecode.pl,
+created by template.el.
+
+It looks like the author of this script was negligent
+enough to leave the stub unedited.
+
+=cut
+
+use 5.10.0;
+use warnings;
+use strict;
+$|=1;
+use Data::Dumper;
+
+print "hey there\n";
+say "but you know";
+
+
+
+__END__
+
+=head1 AUTHOR
+
+Joseph Brenner, E<lt>doom@debian-BULLSEYE-live-builder-AMD64E<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2023 by Joseph Brenner
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.0 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
