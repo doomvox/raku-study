665e2f947b92c3e61ef4db0559026cd0e768f592
Joseph Brenner <doom@kzsu.stanford.edu>
2024-01-21 14:29:06 -0800

    wip

--- bin/2024jan21/chained_substitutions.sh
+++ bin/2024jan21/chained_substitutions.sh
@@ -43,7 +43,8 @@ echo "really?" | raku -pe'.=uc;'
 
 
 # (Mu)
-echo 'roses are red' | raku -pe '.= s/roses/lilacs/'
+# echo 'roses are red' | raku -pe '.= s/roses/lilacs/'
 # Preceding context expects a term, but found infix .= instead.
 
 echo 'roses are red' | raku -pe '.=~ s/roses/lilacs/'
+# Preceding context expects a term, but found infix .= instead.
