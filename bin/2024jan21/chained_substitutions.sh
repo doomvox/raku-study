7f8adc8a7aedf0651b9e22ede7168d0f5ef84cbd
Joseph Brenner <doom@kzsu.stanford.edu>
2024-01-21 14:01:55 -0800

    wip

--- bin/2024jan21/chained_substitutions.sh
+++ bin/2024jan21/chained_substitutions.sh
@@ -2,21 +2,30 @@
 
 
 
-1A.  If you're using `-pe` command line flags, you use `s///` and combine statements with `;` semicolon:
+## 1A and 2A work:
+
+# 1A.  If you're using `-pe` command line flags, you use `s///` and combine statements with `;` semicolon:
 
 ~$ echo 'roses are red' | raku -pe 's/roses/lilacs/;  s/red/blue/'
 lilacs are blue
 
-1B.  Using `-pe` with `s///` but `andthen` instead of `;` is apparently a mistake--you don't get what you expect:
+# 2A.  Conversely, if you're using `-ne` command line flags, you use `S///` and combine statements with `andthen`:
+
+~$ echo 'roses are red' | raku -ne 'S/roses/lilacs/ andthen S/red/blue/.put'
+lilacs are blue
+
+
+
+## 1B and 2B don't work
+
+# 1B.  Using `-pe` with `s///` but `andthen` instead of `;` is apparently a mistake--you don't get what you expect:
 
 ~$ echo 'roses are red' | raku -pe 's/roses/lilacs/ andthen s/red/blue/'
 lilacs are red
 
 
-2A.  Conversely, if you're using `-ne` command line flags, you use `S///` and combine statements with `andthen`:
+## 2A and 2B don't work
 
-~$ echo 'roses are red' | raku -ne 'S/roses/lilacs/ andthen S/red/blue/.put'
-lilacs are blue
 
 2B.  Again, using `-ne` with `S///` but `;` instead of `andthen` is apparently a mistake--you don't get what you expect:
 
