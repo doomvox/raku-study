94069b88b3d4a15464464e33ecc0e38971111e3c
Joseph Brenner <doom@kzsu.stanford.edu>
2024-01-21 14:02:14 -0800

    wip

--- bin/2024jan21/chained_substitutions.sh
+++ bin/2024jan21/chained_substitutions.sh
@@ -6,12 +6,12 @@
 
 # 1A.  If you're using `-pe` command line flags, you use `s///` and combine statements with `;` semicolon:
 
-~$ echo 'roses are red' | raku -pe 's/roses/lilacs/;  s/red/blue/'
+echo 'roses are red' | raku -pe 's/roses/lilacs/;  s/red/blue/'
 lilacs are blue
 
 # 2A.  Conversely, if you're using `-ne` command line flags, you use `S///` and combine statements with `andthen`:
 
-~$ echo 'roses are red' | raku -ne 'S/roses/lilacs/ andthen S/red/blue/.put'
+echo 'roses are red' | raku -ne 'S/roses/lilacs/ andthen S/red/blue/.put'
 lilacs are blue
 
 
@@ -19,7 +19,7 @@ lilacs are blue
 ## 1B and 2B don't work
 # 1B.  Using `-pe` with `s///` but `andthen` instead of `;` is apparently a mistake--you don't get what you expect:
 
-~$ echo 'roses are red' | raku -pe 's/roses/lilacs/ andthen s/red/blue/'
+echo 'roses are red' | raku -pe 's/roses/lilacs/ andthen s/red/blue/'
 lilacs are red
 
 
@@ -27,5 +27,5 @@ lilacs are red
 
 # 2B.  Again, using `-ne` with `S///` but `;` instead of `andthen` is apparently a mistake--you don't get what you expect:
 
-~$ echo 'roses are red' | raku -ne 'S/roses/lilacs/;  S/red/blue/.put'
+echo 'roses are red' | raku -ne 'S/roses/lilacs/;  S/red/blue/.put'
 roses are blue
