b0fe632383b906bc69d38dc73459f901bb6f4efb
Joseph Brenner <doom@kzsu.stanford.edu>
2024-01-21 14:02:26 -0800

    wip

--- bin/2024jan21/chained_substitutions.sh
+++ bin/2024jan21/chained_substitutions.sh
@@ -5,14 +5,13 @@
 ## 1A and 2A work:
 
 # 1A.  If you're using `-pe` command line flags, you use `s///` and combine statements with `;` semicolon:
-
 echo 'roses are red' | raku -pe 's/roses/lilacs/;  s/red/blue/'
-lilacs are blue
+# lilacs are blue
 
 # 2A.  Conversely, if you're using `-ne` command line flags, you use `S///` and combine statements with `andthen`:
 
 echo 'roses are red' | raku -ne 'S/roses/lilacs/ andthen S/red/blue/.put'
-lilacs are blue
+# lilacs are blue
 
 
 
@@ -20,7 +19,7 @@ lilacs are blue
 # 1B.  Using `-pe` with `s///` but `andthen` instead of `;` is apparently a mistake--you don't get what you expect:
 
 echo 'roses are red' | raku -pe 's/roses/lilacs/ andthen s/red/blue/'
-lilacs are red
+# lilacs are red
 
 
 ## 2A and 2B don't work
@@ -28,4 +27,4 @@ lilacs are red
 # 2B.  Again, using `-ne` with `S///` but `;` instead of `andthen` is apparently a mistake--you don't get what you expect:
 
 echo 'roses are red' | raku -ne 'S/roses/lilacs/;  S/red/blue/.put'
-roses are blue
+# roses are blue
