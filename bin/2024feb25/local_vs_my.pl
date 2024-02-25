95ec99cc6cff335bc25d70a02c775f5e0dd4d978
Joseph Brenner <doom@kzsu.stanford.edu>
2024-02-25 13:39:44 -0800

    wip

--- bin/2024feb25/local_vs_my.pl
+++ bin/2024feb25/local_vs_my.pl
@@ -55,6 +55,7 @@ GetOptions ("d|debug"    => \$DEBUG,
 { no warnings 'once'; $DB::single = 1; }
 
 
+
 our $global = 6;
 
 =item do_something
@@ -63,6 +64,7 @@ our $global = 6;
 
 sub do_something {
 
+   $global = 7;
 }
 
   
