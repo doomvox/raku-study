2d072b5403e3057db8c7e5d7ed409b8c9729fb6a
Joseph Brenner <doom@kzsu.stanford.edu>
2024-08-13 11:30:28 -0700

    wip

--- bin/MultiDispatch/f_is_for_faucaultivity.pl
+++ bin/MultiDispatch/f_is_for_faucaultivity.pl
@@ -36,9 +36,10 @@ use Multi::Dispatch;
 
 # exm: examine the arguments
 multi exm ( $i,     $j = 0, $k = 0 ) { guts( "1: ", $i, $j, $k ) };
-# multi exm ( $i,     $j = 0         ) { guts( "2: ", $i, $j, $k ) };
+# multi exm ( $i,     $j = 0         ) { guts( "2: ", $i, $j, $k ) };  # There's no $k!
 multi exm ( $i,     $j = 0         ) { guts( "2: ", $i, $j       ) };
-multi exm ( $i,                    ) { guts( "3: ", $i, $j, $k ) };
+# multi exm ( $i,                    ) { guts( "3: ", $i, $j, $k ) };
+multi exm ( $i,                    ) { guts( "3: ", $i           ) };  # Here just have $i.
 
 # the guts of "exm"
 sub guts ( $label, $i, $j, $k ) {
