56290d4c89625ca3290bbc846bec303ca76700c2
Joseph Brenner <doom@kzsu.stanford.edu>
2024-08-25 12:56:59 -0700

    wip

---
+++ bin/MultiDispatch/broken_a_is_for_arity.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/env perl
+#                                      2024/08/25 19:56:53
+
+=head1 NAME
+
+broken_a_is_for_arity.pl
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+Stub documentation for broken_a_is_for_arity.pl,
+created by template.el.
+
+It looks like the author of this script was negligent
+enough to leave the stub unedited.
+
+=cut
+
+use 5.10.0;
+use warnings;
+use strict;
+$|=1;
+use Data::Dumper;
+
+
+
+
+
+__END__
+
+=head1 AUTHOR
+
+Joseph Brenner, E<lt>doom@debian-BULLSEYE-live-builder-AMD64E<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2024 by Joseph Brenner
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.0 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
