24f2974e35cb0f4f5318d9e48df7daa8831abdb0
Joseph Brenner <tailorpaul@pm.me>
2025-07-13 14:03:44 -0700

    wip

---
+++ bin/2025jul13/list_trig.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/env perl
+#                                      2025/07/13 21:03:24
+
+=head1 NAME
+
+list_trig.pl
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+Stub documentation for list_trig.pl,
+created by template.el.
+
+It looks like the author of this script was negligent
+enough to leave the stub unedited.
+
+=cut
+
+use 5.10.0;
+use warnings;
+use strict;
+$|=1;
+use Data::Dumper;
+
+my @a = (1, 2, 3);
+say @a.sin;
+
+
+
+
+__END__
+
+=head1 AUTHOR
+
+Joseph Brenner, E<lt>doom@debian-BULLSEYE-live-builder-AMD64E<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2025 by Joseph Brenner
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.0 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
