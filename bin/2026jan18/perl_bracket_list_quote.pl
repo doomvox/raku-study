44557d0bf6cb7b88e8157dce03f69412236b7310
Joseph Brenner <tailorpaul@pm.me>
2026-01-18 14:24:38 -0800

    wip

---
+++ bin/2026jan18/perl_bracket_list_quote.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/env perl
+#                                      2026/01/18 22:24:23
+
+=head1 NAME
+
+perl_bracket_list_quote.pl
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+Stub documentation for perl_bracket_list_quote.pl,
+created by template.el.
+
+It looks like the author of this script was negligent
+enough to leave the stub unedited.
+
+=cut
+
+use 5.10.0;
+use warnings;
+use strict;
+$|=1;
+use Data::Dumper;
+
+
+my @words = <alpha beta gama>;
+
+
+
+
+
+__END__
+
+=head1 AUTHOR
+
+Joseph Brenner, E<lt>doom@debian-BULLSEYE-live-builder-AMD64E<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2026 by Joseph Brenner
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.0 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
