3b820281a0e20fe20e7a1db1158c0f4fad02adeb
Joseph Brenner <tailorpaul@pm.me>
2026-05-04 20:21:51 -0700

    wip

--- bin/2026may10/perl_flattening.pl
+++ bin/2026may10/perl_flattening.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/env perl
 
+use List::Util;
 
 my @monsters = < basilisk minotaur behemoth leviathan ziz chimera manticore >;
 my @levels   =  (6,       5,       7,       8,         4,   3,       4); 
