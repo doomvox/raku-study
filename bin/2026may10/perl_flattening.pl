8ab2706f8911c93a57acf84a8f1c103f7fcd7a19
Joseph Brenner <tailorpaul@pm.me>
2026-05-04 20:22:57 -0700

    wip

--- bin/2026may10/perl_flattening.pl
+++ bin/2026may10/perl_flattening.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 
-use List::Util;
+use List::Util qw(pairmap);
 
 my @monsters = < basilisk minotaur behemoth leviathan ziz chimera manticore >;
 my @levels   =  (6,       5,       7,       8,         4,   3,       4); 
