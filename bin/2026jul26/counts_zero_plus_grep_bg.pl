f03fe6f83e1e6a88e0efce529a5f5acf4141078e
Joseph Brenner <tailorpaul@pm.me>
2026-07-20 11:26:56 -0700

    wip

--- bin/2026jul26/counts_zero_plus_grep_bg.pl
+++ bin/2026jul26/counts_zero_plus_grep_bg.pl
@@ -83,7 +83,6 @@ my @pets = qw<dog cat dog cat dog dog>;
 
 
 { # pattern match instead of eq
-  # https://files.speakerdeck.com/presentations/f6b5c3e61fab4a3b8635f16afdb6cc6b/grouping_unclassified_-_tprc_2026.pdf
   my $dogs = 0+grep { /dog/ } @pets;
   my $cats = 0+grep { /cat/ } @pets;
 
