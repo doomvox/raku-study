982bef5e8b2d029d37972ea9314c12656a5301bf
Joseph Brenner <tailorpaul@pm.me>
2026-07-20 11:25:58 -0700

    wip

--- bin/2026jul26/counts_zero_plus_grep_bg.pl
+++ bin/2026jul26/counts_zero_plus_grep_bg.pl
@@ -67,7 +67,8 @@ GetOptions ("d|debug"    => \$DEBUG,
 { no warnings 'once'; $DB::single = 1; }
 
 my @pets = qw<dog cat dog cat dog dog>;
-{
+{ # bruce gray slide, 
+  # https://files.speakerdeck.com/presentations/f6b5c3e61fab4a3b8635f16afdb6cc6b/grouping_unclassified_-_tprc_2026.pdf
   my $dogs = 0+grep { $_ eq 'dog' } @pets;
   my $cats = 0+grep { $_ eq 'cat' } @pets;
 
