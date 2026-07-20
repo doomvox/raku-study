8eaec0b8014633afe6f5fabf3de3a3f10f296387
Joseph Brenner <tailorpaul@pm.me>
2026-07-20 11:25:41 -0700

    wip

--- bin/2026jul26/counts_zero_plus_grep_bg.pl
+++ bin/2026jul26/counts_zero_plus_grep_bg.pl
@@ -66,8 +66,8 @@ GetOptions ("d|debug"    => \$DEBUG,
 
 { no warnings 'once'; $DB::single = 1; }
 
+my @pets = qw<dog cat dog cat dog dog>;
 {
-  my @pets = qw<dog cat dog cat dog dog>;
   my $dogs = 0+grep { $_ eq 'dog' } @pets;
   my $cats = 0+grep { $_ eq 'cat' } @pets;
 
